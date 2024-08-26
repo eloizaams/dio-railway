04a30bf46ffd627c7672526312c6020466a7da14
eloizaams <eloizaams@yahoo.com.br>
2024-08-26 11:49:29 -0300

    feat: implementacao camada dominio

---
+++ nuvem/src/main/java/eloiza/nuvem/domain/model/Feature.java
@@ -0,0 +1,9 @@
+package eloiza.nuvem.domain.model;
+
+import jakarta.persistence.*;
+
+@Entity(name="tb_feature")
+public class Feature extends BaseItem{
+	
+
+}
---
+++ nuvem/src/main/java/eloiza/nuvem/domain/model/News.java
@@ -0,0 +1,9 @@
+package eloiza.nuvem.domain.model;
+
+import jakarta.persistence.*;
+
+@Entity(name="tb_news")
+public class News extends BaseItem {
+	
+	
+}
